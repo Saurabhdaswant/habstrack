5aca03fbcbe658d8083c73f802311173dba57636
saurabh <77434364+Saurabhdaswant@users.noreply.github.com>
2024-04-07 10:18:02 +0530

    .

--- client/nice.js
+++ client/nice.js
@@ -1,7 +1,3 @@
 // - setup backend
 // - connect backend to server / database
 // - import frontend from headway
-
-// password : YbOXMlokhDnv0XPc
-// MONGODB_URI : mongodb+srv://daswant99:YbOXMlokhDnv0XPc@cluster0.zwshtoo.mongodb.net/
-// connection string : mongodb+srv://daswant99:<password>@cluster0.zwshtoo.mongodb.net/
